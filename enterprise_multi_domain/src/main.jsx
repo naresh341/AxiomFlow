f420bfc2b6a1401d56f807ecb4dd9008716c6347
Naresh Bhati <bhati.naresh43@gmail.com>
2026-08-31 15:35:48 +0530

    fix:UI

--- enterprise_multi_domain/src/main.jsx
+++ enterprise_multi_domain/src/main.jsx
@@ -2,15 +2,15 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./index.css";
-import { Store } from "./Store/Store.js";
-import { ThemeProvider } from "./Context/ThemeContext.jsx";
 import "primereact/resources/themes/lara-light-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
+import "react-loading-skeleton/dist/skeleton.css";
+import "./index.css";
+import { Store } from "./Store/Store.js";
+import { ThemeProvider } from "./Context/ThemeContext.jsx";
 import { PrimeReactProvider } from "primereact/api";
 import GlobalNotificationModal from "./Components/MiniComponent/GlobalNotificationModal.jsx";
-import "react-loading-skeleton/dist/skeleton.css";
 const primeReactConfig = {
   ripple: true,
   hideOverlaysOnDocumentScrolling: true,
